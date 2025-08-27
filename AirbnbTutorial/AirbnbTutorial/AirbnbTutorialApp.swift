1de0cfb5be828899a0f97b652e429aab5a918ca8
Dayasri 007 <dayasri007@Mac.mshome.net>
2025-08-27 11:02:31 +0530

    initial commit

---
+++ AirbnbTutorial/AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Dayasri 007 on 2025-08-27.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbTutorial/AirbnbTutorialTests/AirbnbTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialTests.swift
+//  AirbnbTutorialTests
+//
+//  Created by Dayasri 007 on 2025-08-27.
+//
+
+import Testing
+@testable import AirbnbTutorial
+
+struct AirbnbTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
