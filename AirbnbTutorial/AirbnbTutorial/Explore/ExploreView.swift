90ec1698d91b7149382739837c0eae42bafba634
Dayasri 007 <dayasri007@Mac.mshome.net>
2025-08-27 13:26:37 +0530

    create search and fileter bar and update ExploreView swif file

--- AirbnbTutorial/AirbnbTutorial/Explore/ExploreView.swift
+++ AirbnbTutorial/AirbnbTutorial/Explore/ExploreView.swift
@@ -4,16 +4,20 @@ import SwiftUI
 struct ExploreView:View {
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVStack {
-                    ForEach(0..<20,id:\.self) {
-                      
-                        listing in ListingItemView()
-                            .frame(height: 400)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
+            VStack {
+                SearchAndFilterBar()
+                ScrollView {
+                    
+                    LazyVStack {
+                        ForEach(0..<20,id:\.self) {
+                            
+                            listing in ListingItemView()
+                                .frame(height: 400)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
                     }
+                    .padding()
                 }
-                .padding()
             }
         }
     }
